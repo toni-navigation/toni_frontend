70d9da4aca2e4872a661546199683aabd8e12ec1
Pavithira Manoharan <pavithira.manoharan@hdi.de>
2024-02-01 16:54:52 +0100

    Change Component/Atoms/Button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-interface PrimaryButtonProps {
+interface ButtonProps {
   children: React.ReactNode;
   onPress: () => void;
+  buttonType: 'primary' | 'secondary' | 'primaryOutline' | 'secondaryOutline';
   disabled?: boolean;
 }
-function Button({ children, onPress, disabled }: PrimaryButtonProps) {
+function Button({ children, onPress, buttonType, disabled }: ButtonProps) {
+  let buttonLook;
+  switch (buttonType) {
+    case 'primary':
+      buttonLook = 'bg-green-800 hover:bg-green-950';
+      break;
+    case 'secondary':
+      buttonLook = 'bg-yellow-800 hover:bg-yellow-950';
+      break;
+    case 'primaryOutline':
+      buttonLook =
+        'bg-green-200 hover:bg-green-950 border border-solid border-black-600';
+      break;
+    case 'secondaryOutline':
+      buttonLook =
+        'bg-yellow-200 hover:bg-green-950 border border-solid border-black-600';
+      break;
+    default:
+      buttonLook = 'bg-gray-500 hover:bg-gray-700';
+      break;
+  }
+
   return (
     <TouchableOpacity
-      className={`h-20 flex justify-center font-bold py-2 px-4 rounded ${!disabled ? 'bg-green-800 hover:bg-green-950' : 'bg-amber-400 hover:bg-green-950'}}`}
+      className={`h-20 flex justify-center font-bold py-2 px-4 rounded ${!disabled ? buttonLook : 'bg-amber-400 hover:bg-amber-950'}}`}
       onPress={onPress}
       disabled={disabled}
     >
