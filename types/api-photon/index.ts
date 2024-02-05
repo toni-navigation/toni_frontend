4ccfea5a53e6e38d9137c11deb16a4ef6ad71592
Julian Fleig <julian.fleig@hs-furtwangen.de>
2024-02-05 15:04:17 +0100

    Add Photon Types

---
+++ types/api-photon/index.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { PhotonFeature } from './models/PhotonFeature';
+export type { PhotonFeatureCollection } from './models/PhotonFeatureCollection';
+
+export { PhotonService } from './services/PhotonService';
---
+++ types/api-photon/models/PhotonFeature.ts
@@ -0,0 +1,26 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PhotonFeature = {
+    type: 'Feature';
+    geometry: {
+        type: 'Point';
+        coordinates: Array<number>;
+    };
+    properties: {
+        osm_id?: number;
+        osm_type?: string;
+        extent?: Array<number>;
+        country?: string;
+        osm_key?: string;
+        city?: string;
+        osm_value?: string;
+        postcode?: string;
+        name?: string;
+        state?: string;
+        street?: string;
+        housenumber?: string;
+    };
+};
+
