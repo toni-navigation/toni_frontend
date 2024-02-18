d9c5d49fc5f013ef2f7892d1b7de8ba49b24861c
Pavithira Manoharan <pavithira.manoharan@hdi.de>
2024-02-18 16:51:48 +0100

    Edit tailwind.config.js & Dark/LightMode Experiment Button.tsx

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,43 +1,111 @@
 import React from 'react';
+import { Text, TouchableOpacity, useColorScheme } from 'react-native';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onPress: () => void;
+  buttonType: 'primary' | 'secondary' | 'primaryOutline' | 'secondaryOutline';
+  disabled?: boolean | false;
+}
+function Button({ children, onPress, buttonType, disabled }: ButtonProps) {
+  const colorscheme = useColorScheme();
+  console.log(colorscheme);
+  let buttonLook;
+  switch (buttonType) {
+    case 'primary':
+      buttonLook =
+        colorscheme === 'light'
+          ? 'bg-primary-color-light'
+          : 'bg-primary-color-dark';
+      break;
+    case 'secondary':
+      buttonLook =
+        colorscheme === 'light'
+          ? 'bg-secondary-color-light'
+          : 'bg-secondary-color-dark';
+      break;
+    case 'primaryOutline':
+      buttonLook =
+        colorscheme === 'light'
+          ? 'bg-primary-color-light border border-solid border-black-600'
+          : 'bg-primary-color-dark border border-solid border-black-600';
+      break;
+    case 'secondaryOutline':
+      buttonLook =
+        colorscheme === 'light'
+          ? 'bg-secondary-color-light border border-solid border-black-600'
+          : 'bg-secondary-color-dark border border-solid border-black-600';
+      break;
+    default:
+      buttonLook = 'bg-primary-color-light';
+      break;
+  }
+
+  return (
+    <TouchableOpacity
+      className={`h-20 flex justify-center font-bold py-2 px-4 rounded ${disabled ? 'bg-disabled-color' : buttonLook}`}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Text
+        className="(colorscheme === 'light'
+          ? 'text-white font-verdana'
+          : 'text-dark font-verdana')
+          text-white text-center text-lg"
+      >
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+export default Button;
+
+/*import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 interface ButtonProps {
   children: React.ReactNode;
   onPress: () => void;
   buttonType: 'primary' | 'secondary' | 'primaryOutline' | 'secondaryOutline';
-  disabled?: boolean;
+  disabled?: boolean | false;
 }
 function Button({ children, onPress, buttonType, disabled }: ButtonProps) {
   let buttonLook;
   switch (buttonType) {
     case 'primary':
-      buttonLook = 'bg-primary-color hover:bg-primary-color-950';
+      buttonLook = 'bg-primary-color-light dark:bg-primary-color-dark';
       break;
     case 'secondary':
-      buttonLook = 'bg-yellow-800 hover:bg-yellow-950';
+      buttonLook = 'bg-secondary-color-light dark:bg-secondary-color-dark';
       break;
     case 'primaryOutline':
       buttonLook =
-        'bg-green-200 hover:bg-green-950 border border-solid border-black-600';
+        'bg-primary-color-light border border-solid border-black-600 dark:bg-primary-color-dark border border-solid border-black-600';
       break;
     case 'secondaryOutline':
       buttonLook =
-        'bg-yellow-200 hover:bg-green-950 border border-solid border-black-600';
+        'bg-secondary-color-light border border-solid border-black-600 dark:bg-secondary-color-dark border border-solid border-black-600';
       break;
     default:
-      buttonLook = 'bg-gray-500 hover:bg-gray-700';
+      buttonLook = 'bg-primary-color-light';
       break;
   }
 
   return (
     <TouchableOpacity
-      className={`h-20 flex justify-center font-bold py-2 px-4 rounded ${!disabled ? buttonLook : 'bg-amber-400 hover:bg-amber-950'}}`}
+      className={`h-20 flex justify-center font-bold py-2 px-4 rounded ${disabled ? 'bg-disabled-color' : buttonLook}`}
       onPress={onPress}
       disabled={disabled}
     >
-      <Text className="text-white text-center text-lg">{children}</Text>
+      <Text
+        className="('text-white font-verdana dark:text-black font-verdana')
+          text-white text-center text-lg"
+      >
+        {children}
+      </Text>
     </TouchableOpacity>
   );
 }
 
-export default Button;
+export default Button;*/
