239a35b252119e7f9b08f664505b348bc75ff4d1
Pavithira Manoharan <pavithira.manoharan@hdi.de>
2024-02-29 16:02:11 +0100

    TabView Styling

---
+++ src/components/organisms/TabBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { NavigationState } from 'react-native-tab-view';
+
+interface TabBarProps {
+  jumpTo: (key: string) => void;
+  navigationState: NavigationState<{ key: string; title: string }>;
+}
+function TabBar({ jumpTo, navigationState }: TabBarProps) {
+  const activeButton =
+    'h-20 flex justify-center py-2 px-4 rounded bg-primary-color-light flex-1 mr-2';
+  const inactiveButton =
+    'h-20 flex justify-center py-2 px-4 rounded bg-background-light flex-1';
+  const activeText = 'text-center text-base text-background-light';
+  const inactiveText = 'text-center text-base text-background-dark';
+
+  return (
+    <View className="flex-row">
+      {navigationState.routes.map((route, index) => (
+        <TouchableOpacity
+          className={
+            index === navigationState.index ? activeButton : inactiveButton
+          }
+          onPress={() => jumpTo(route.key)}
+        >
+          <Text
+            className={
+              index === navigationState.index ? activeText : inactiveText
+            }
+          >
+            {route.title}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+export default TabBar;
--- src/pages/Trip.tsx
+++ src/pages/Trip.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { ValhallaManeuverProps } from '../../types/Valhalla-Types';
-import MapView, { Polyline } from 'react-native-maps';
 
 interface Trip {
   maneuver: ValhallaManeuverProps;
@@ -9,7 +8,7 @@ interface Trip {
 }
 function Trip({ factor, maneuver }: Trip) {
   const tripText = `${maneuver.instruction} ${maneuver.length * 1000} Meter`;
-  console.log(factor);
+
   return (
     <View className="border-b-2">
       <Text>{tripText}</Text>
