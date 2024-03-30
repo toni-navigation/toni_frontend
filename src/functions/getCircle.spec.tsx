4ff3eaf5ade1c8539224d0dab977db97ab09772b
Julian Fleig <fleig.julian@googlemail.com>
2024-03-30 13:21:36 +0100

    Add Bbox

---
+++ src/functions/getCircle.spec.tsx
@@ -0,0 +1,25 @@
+import circle from '@turf/circle';
+import * as turf from '@turf/helpers';
+import { Units } from '@turf/helpers';
+
+describe('getCircle', () => {
+  it('should return a circle with the correct radius', () => {
+    const center = [-75.343, 39.984];
+    const radius = 5;
+    const options = {
+      steps: 4,
+      units: 'kilometers' as Units,
+    };
+    const perimeter = circle(center, radius, options);
+    // const circle = getCircle(10);
+    expect(perimeter.geometry.coordinates).toBe([
+      [
+        [-75.343, 40.02896601818623],
+        [-75.40168521055865, 39.983985202669075],
+        [-75.343, 39.93903398181377],
+        [-75.28431478944134, 39.983985202669075],
+        [-75.343, 40.02896601818623],
+      ],
+    ]);
+  });
+});
--- src/queries/useGeocoding.ts
+++ src/queries/useGeocoding.ts
@@ -5,12 +5,15 @@ import { PhotonService } from '@/services/api-photon';
 export function useGeocoding(
   debouncedInputValue: string,
   focused: boolean,
+  bbox?: number[][],
   limit = 5
 ) {
+  console.log(bbox);
   const parameters: Parameters<typeof PhotonService.geocoding>[0] = {
     q: debouncedInputValue,
     limit,
     lang: 'de',
+    ...(bbox ? { bbox } : {}),
   };
 
   return useQuery({
