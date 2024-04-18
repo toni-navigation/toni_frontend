583105c1735a193001059c896d99422c8b3300e5
Julian Fleig <fleig.julian@googlemail.com>
2024-04-18 20:38:39 +0200

    Import Ionicons differently

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import React, { useEffect } from 'react';
 import {
