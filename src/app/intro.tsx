2f6e628e52653a57e7441d9bcd77757abf905260
Philine Hoernemann <phhoernemann@web.de>
2024-04-26 12:18:54 +0200

    change intro page and Logo component

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -1,5 +1,5 @@
 import { router } from 'expo-router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -12,42 +12,107 @@ import { Button } from '@/components/atoms/Button';
 import { Header } from '@/components/atoms/Header';
 import { Logo } from '@/components/atoms/Logo';
 
-export default function CalibrationPage() {
+export default function IntroPage() {
   const colorscheme = useColorScheme();
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const toggle = setInterval(() => {
+      if (index === 0) {
+        setIndex(1);
+      }
+      if (index === 1) {
+        setIndex(2);
+      }
+      if (index === 2) {
+        setIndex(0);
+      }
+    }, 3000);
+
+    return () => clearInterval(toggle);
+  });
 
   return (
     <SafeAreaView
       className={`flex-1 ${colorscheme === 'light' ? 'bg-background-light' : 'bg-background-dark'}`}
     >
-      <ScrollView className="mx-8 my-8">
-        <View className="flex items-center pb-6">
-          <Logo mode={colorscheme} size={85} />
-        </View>
-        <Header>Schrittlänge konfigurieren</Header>
-        <View>
-          <Text className="text-2xl font-atkinsonRegular">
-            Nun kalibrieren wir deine Schrittlänge, damit die Navigation
-            möglichst genau wird. Bitte stelle im Vorfeld sicher, dass die
-            Strecke geeignet ist, also möglichst gerade und ohne Hindernisse.
-            Solltest du dir unsicher sein, bitte eine andere Person um Hilfe.
-            Drücke nun auf den Weiter Button und klicke dort auf &quot;Start
-            Kalibrierung&quot; und gehe so lange gerade aus bis die Musik
-            stoppt.
-          </Text>
-        </View>
-        <Button
-          buttonType="primary"
-          disabled={false}
-          onPress={() => {
-            const params = {
-              fromProfile: 0,
-            };
-            router.push({ pathname: '/profile/calibration', params });
-          }}
-        >
-          Weiter
-        </Button>
+      <ScrollView className="px-8 pt-20 ">
+        {index === 0 && (
+          <>
+            <View className="flex items-center pb-8">
+              <Logo
+                icon={`${colorscheme === 'light' ? 'logoLight' : 'logoDark'}`}
+                size={115}
+              />
+            </View>
+            <View className="flex items-center">
+              <Header>Dein Weg</Header>
+              <Text
+                className={`mx-auto text-center font-atkinsonRegular text-xl ${colorscheme === 'light' ? 'text-text-color-light' : 'text-background-light'}`}
+              >
+                Toni führt dich, auf deine Schrittlänge konfiguriert, sicher an
+                dein Ziel!
+              </Text>
+            </View>
+          </>
+        )}
+        {index === 1 && (
+          <>
+            <View className="flex items-center pb-8">
+              <Logo
+                icon={`${colorscheme === 'light' ? 'logoLight' : 'logoDark'}`}
+                size={115}
+              />
+            </View>
+            <View className="flex items-center">
+              <Header>Dein Klang</Header>
+              <Text
+                className={`mx-auto text-center font-atkinsonRegular text-xl ${colorscheme === 'light' ? 'text-text-color-light' : 'text-background-light'}`}
+              >
+                Entscheide individuell welche Stimme dich auf deinem Weg
+                begleitet!
+              </Text>
+            </View>
+          </>
+        )}
+        {index === 2 && (
+          <>
+            <View className="flex items-center pb-8">
+              <Logo
+                icon={`${colorscheme === 'light' ? 'logoLight' : 'logoDark'}`}
+                size={115}
+              />
+            </View>
+            <View className="flex items-center">
+              <Header>Deine Freiheit</Header>
+              <Text
+                className={`mx-auto text-center font-atkinsonRegular text-xl ${colorscheme === 'light' ? 'text-text-color-light' : 'text-background-light'}`}
+              >
+                Für mehr Leichtigkeit und Selbständigkeit in deinem Alltag!
+              </Text>
+            </View>
+          </>
+        )}
       </ScrollView>
+      <View className="mx-8 mb-3">
+        <>
+          <Button buttonType="accent" disabled onPress={() => {}}>
+            Registrieren
+          </Button>
+          <Button
+            buttonType="primary"
+            disabled={false}
+            onPress={() => {
+              const params = {
+                fromProfile: 0,
+              };
+              router.push({ pathname: '/profile/calibration', params });
+            }}
+          >
+            Los gehts
+          </Button>
+        </>
+      </View>
     </SafeAreaView>
   );
 }
--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,18 +1,13 @@
-import { ColorSchemeName } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 
 import * as icons from '@/assets/icons/icons';
+import { IconByKey } from '@/components/atoms/Icon';
 
 export interface LogoProps {
-  mode: ColorSchemeName;
   size?: number;
+  icon: IconByKey;
 }
 
-export function Logo({ mode, size }: LogoProps) {
-  if (mode === 'light') {
-    return <SvgXml xml={icons.logoLight} width={size} height={size} />;
-  }
-  if (mode === 'dark') {
-    return <SvgXml xml={icons.logoDark} width={size} height={size} />;
-  }
+export function Logo({ icon, size }: LogoProps) {
+  return <SvgXml xml={icons[icon]} width={size} height={size} />;
 }
