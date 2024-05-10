56cb4d0ba738185db02b3aef0c388a341ef755bb
Philine Hoernemann <phhoernemann@web.de>
2024-05-10 19:08:48 +0200

    add react-native-reanimated

--- src/app/(tabs)/home/_layout.tsx
+++ src/app/(tabs)/home/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from 'expo-router';
+import 'react-native-reanimated';
 
 function HomeLayout() {
   return (
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
+import 'react-native-reanimated';
 
 import generalSansSemi from '@/assets/fonts/GeneralSans-Semibold.otf';
 
