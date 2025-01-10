d1929a61cf1f7fa4f2db419e48fc788436022f10
Julian Fleig <fleig.julian@googlemail.com>
2025-01-10 21:49:59 +0100

    Update Geocoding Search

--- src/queries/useGeocoding.ts
+++ src/queries/useGeocoding.ts
@@ -1,29 +1,37 @@
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 import { getBbox } from '@/functions/getBbox';
+import { getCurrentPosition } from '@/functions/getCurrentPosition';
 import { PhotonService } from '@/services/api-photon';
-import { useCurrentLocationStore } from '@/store/useCurrentLocationStore';
 
 export function useGeocoding(
   debouncedInputValue: string,
   focused: boolean,
   limit = 5
 ) {
-  const currentLocation = useCurrentLocationStore(
-    (state) => state.currentLocation
-  );
-  const defaultBbox = currentLocation && getBbox(currentLocation);
+  // const currentLocation = useCurrentLocationStore(
+  //   (state) => state.currentLocation
+  // );
+  // const defaultBbox = currentLocation && getBbox(currentLocation);
 
   const parameters: Parameters<typeof PhotonService.geocoding>[0] = {
     q: debouncedInputValue,
     limit,
     lang: 'de',
-    ...(defaultBbox ? { bbox: defaultBbox.join(',') } : {}),
+    // ...(defaultBbox ? { bbox: defaultBbox.join(',') } : {}),
   };
 
   return useQuery({
     queryKey: ['photon', parameters],
-    queryFn: () => PhotonService.geocoding(parameters),
+    queryFn: async () => {
+      const currentLocation = await getCurrentPosition();
+      const defaultBbox = currentLocation && getBbox(currentLocation);
+
+      return PhotonService.geocoding({
+        ...parameters,
+        ...(defaultBbox ? { bbox: defaultBbox.join(',') } : {}),
+      });
+    },
     enabled: focused,
     placeholderData: keepPreviousData,
   });
